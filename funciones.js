8f7cf20fdfca2aa798ddd2278b88f06b167e8210
1franken <francoexequiel_garcia@hotmail.com>
2023-02-15 14:31:38 -0300

    reinicio

--- funciones.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('boton').onclick = function() {
-    document.getElementById("demo").innerHTML = "0341-156991069";
-}
-document.getElementById('boton2').onclick = function() {
-    document.getElementById("demo2").innerHTML = "francoexequiel.garcia150@gmail.com";
-}
